a0558f83920213a89db16ea9d7cd927839c24a00
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2022-12-01 00:59:23 +0000

    extracting some ports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::net::TcpListener;
+
+use actix_web::dev::Server;
 use zero::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    run()?.await
-}
+async fn main() -> Result<((), ())> {
 
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    zero::run( listener ).expect("Failed to bind address")
+
+
+}
