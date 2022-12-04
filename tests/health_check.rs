fa637986f97cec37e2cbf653803fddbf63fc69ca
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2022-12-04 22:48:59 +0000

    initial invalid body

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -44,6 +44,29 @@ async fn subscribe_returns_200_for_valid_form () {
     assert!(response.status().is_success());
 }
 
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_form () {
+    // Arrange
+    let app_address = spawn_app();
+
+    let client = reqwest::Client::new();
+    let body = "&email=mcm@matthewcm.dev";
+
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-wwww-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+
+    // Assert
+    assert_eq!(400, response.status().as_u16(), "Invalid body");
+
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
