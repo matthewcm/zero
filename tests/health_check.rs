5a3b153273e1735d3542a7f6b3056632ea5174e8
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2022-12-04 22:44:01 +0000

    tests: TDD subscriber tets

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,6 +22,28 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form () {
+    // Arrange
+    let app_address = spawn_app();
+
+    let client = reqwest::Client::new();
+    let body = "name=Matthew%20Castrillon&email=mcm@matthewcm.dev";
+
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-wwww-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+
+    // Assert
+    assert!(response.status().is_success());
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
